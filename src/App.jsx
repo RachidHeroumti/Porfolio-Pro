68efc3b55ff5f1f8bd03f2b797c21c2a1b717c35
rachidcooder <cooderherou@gmail.com>
2023-12-30 11:17:15 +0000

    ignor f3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
 import Portfolio1 from './TestPotfolios/Portfolio1'
-import Portfolio3 from './TestPotfolios/Porfolio3'
 
 function App() {
   return (
     <div >
-  <Portfolio1/>
+      <Portfolio1 />
     </div>
   )
 }
